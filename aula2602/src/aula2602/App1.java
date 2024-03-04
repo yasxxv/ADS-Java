add2ee475f82f6de2924b9f65dd19fa936b0769c
Yasmin Benicio <79879086+yasxxv@users.noreply.github.com>
2024-03-04 20:22:21 -0300

    Add files via upload

---
+++ aula2602/src/aula2602/App1.java
@@ -0,0 +1,20 @@
+package aula2602;
+import java.util.*;
+
+public class App1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Digite um nome: ");
+		String nome = sc.nextLine();
+		System.out.println("Nome: " + nome);
+		
+		System.out.println("Digite uma idade: ");
+		int idade = sc.nextInt();
+		
+		System.out.println("Digite seu salário: ");
+		double salario = sc.nextDouble();
+		
+	}
+
+}
